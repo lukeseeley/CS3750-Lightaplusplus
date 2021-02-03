7b4285ec6c4ad3e63b465523d2766edcac0c54c6
Jesen Meznarich <jesenmeznarich@mail.weber.edu>
2021-02-03 16:38:52 -0700

    Validation on Birthday and Confirmed Password
    Know it will validate that you are older than 18 years old and check to make sure your passwords match.

---
+++ User Management System/Models/MinimumAgeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User_Management_System.Models
+{
+    public class MinimumAgeAttribute : ValidationAttribute
+    {
+        public int MinimumAge { get; }
+
+        public string GetErrorMessage() =>
+            $"You must be at least {MinimumAge} years old.";
+
+        public MinimumAgeAttribute(int minimumAge)
+        {
+            MinimumAge = minimumAge;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            
+            var Birthday = (DateTime)value;
+            
+            // get how old they are
+            DateTime age = Birthday.AddYears(MinimumAge);
+
+            // check to see how old they are
+            if (age > DateTime.Now)
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+            return ValidationResult.Success;
+        }
+
+    }
+}
