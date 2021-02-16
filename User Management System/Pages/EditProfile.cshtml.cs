e0be8303efe6c64490a88233177e76ea6d3a5f63
Ben-Mullins <48454984+Ben-Mullins@users.noreply.github.com>
2021-02-15 19:11:15 -0700

    Added some dawt net

--- User Management System/Pages/EditProfile.cshtml.cs
+++ User Management System/Pages/EditProfile.cshtml.cs
@@ -4,13 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lightaplusplus.Models;
 
 namespace Lightaplusplus.Pages
 {
     public class EditProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly Lightaplusplus.Data.Lightaplusplus_SystemContext _context;
+
+        public EditProfileModel(Lightaplusplus.Data.Lightaplusplus_SystemContext context)
         {
+            _context = context;
+        }
+
+        public Users Users { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Users = await _context.Users.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Users == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
+}
 }
