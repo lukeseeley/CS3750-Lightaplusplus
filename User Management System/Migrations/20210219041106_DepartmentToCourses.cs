5435be0b46705739b4e825408d3b592293a73d7c
lukeseeley <luke@seeleyone.com>
2021-02-18 21:16:33 -0700

    Added Department to Course

---
+++ User Management System/Migrations/20210219041106_DepartmentToCourses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lightaplusplus.Migrations
+{
+    public partial class DepartmentToCourses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CourseDepartment",
+                table: "Courses",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseDepartment",
+                table: "Courses");
+        }
+    }
+}
