174f6bbca35c91eba90607030feaca730936efeb
crsherwood <cadesherwood@mail.weber.edu>
2021-02-24 06:15:58 -0700

    Worked on grid display. Tried to finish register button and search bar.

--- User Management System/Pages/Registration/Index.cshtml.cs
+++ User Management System/Pages/Registration/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Lightaplusplus.Data;
 using Lightaplusplus.Models;
@@ -19,15 +20,42 @@ namespace Lightaplusplus.Pages.Registration
             _context = context;
         }
 
-        public IList<Sections> Sections { get; set; }
+        [BindProperty]
+        public int StudentId { get; set; }
 
-        public async Task OnGetAsync()
+        [BindProperty]
+        public List<Models.Sections> SectionsList { get; set; }
+
+        public string[] Departments = new string[] { "Accounting", "Art", "Biology", "Chemistry", "Computer Science", "Engineering", "English", "Health Science", "History", "Mathematics", "Music", "Social Science", "Physics" };
+        
+
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
-            Sections = await _context.Sections
+            if (id == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (user == null)
+            {
+                return RedirectToPage("/Index");
+            }
+            if (user.usertype != 'S') // Make sure only the student is viewing registration
+            {
+                return RedirectToPage("/Welcome", new { id= id });
+            }
+
+            StudentId = (int)id;
+
+            SectionsList = await _context.Sections
                 .Include(s => s.Instructor)
                 .Include(s => s.Course)
                 .AsNoTracking()
                 .ToListAsync();
+
+            return Page();
         }
     }
 }
