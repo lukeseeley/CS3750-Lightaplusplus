baa2cb0d0d9fb00f5a36b036014e2a489b40d470
lukeseeley <luke@seeleyone.com>
2021-04-01 15:46:53 -0600

    Notification Class Overhaul

--- User Management System/BisLogic/Notifications.cs
+++ User Management System/BisLogic/Notifications.cs
@@ -1,5 +1,6 @@
 ﻿using Lightaplusplus.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -8,34 +9,72 @@ using System.Threading.Tasks;
 
 namespace Lightaplusplus.BisLogic
 {
-    public static class Notifications
+    public class Notifications
     {
-        public static void SetGrades(this ISession session, string grades)
-        {
-            session.SetString("Grades", grades);
-        }
+        public List<Notification> NotificationList { get; set; }
 
-        public static List<Grades> GetGrades(this ISession session)
+        public Notifications(ISession session, Lightaplusplus.Data.Lightaplusplus_SystemContext context)
         {
-            string myGrades = session.GetString("Grades");
-            List<Grades> gradesList = new List<Grades>();
-            foreach (var grade in myGrades.Split(":::"))
+            var id = Session.getUserId(session);
+            var usertype = Session.getUserType(session);
+            var currentLogin = Session.getUserCurrentLogin(session);
+            var lastLogin = Session.getUserLastLogin(session);
+            var sections = Session.getAllSections(session);
+
+            if (lastLogin == null)
             {
-                try
-                {
-                    if (grade.Length > 0)
-                    {
-                        Grades myGrade = JsonConvert.DeserializeObject<Grades>(grade);
-                        gradesList.Add(myGrade);
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
+                lastLogin = DateTime.Parse("1/1/1980");
             }
 
-            return gradesList;
+            NotificationList = new List<Notification>();
+
+            if(usertype == "S")
+            {
+                foreach (var section in sections)
+                {
+                    var GradeList = context.Grades.Include(g => g.Assignment).Where(g => g.StudentId == id && g.Assignment.SectionId == section.SectionId && g.GradeDateTime > (DateTime)lastLogin).ToList();
+                    var AssignmentList = context.Assignments.Where(a => a.SectionId == section.SectionId && a.AssignmentCreationDate != null && a.AssignmentCreationDate > lastLogin).ToList();
+
+                    foreach (var grade in GradeList)
+                    {
+                        NotificationList.Add(new Notification(grade.Assignment.AssignmentTitle, 'G', section.Course.CourseCode + " " + section.Course.CourseNumber, $"/Courses/{section.SectionId}", grade.GradeDateTime));
+                    }
+                    foreach (var assignment in AssignmentList)
+                    {
+                        NotificationList.Add(new Notification(assignment.AssignmentTitle, 'A', section.Course.CourseCode + " " + section.Course.CourseNumber, $"/Courses/{section.SectionId}/Assignments/{assignment.AssignmentId}", (DateTime)assignment.AssignmentCreationDate));
+                    }
+                }
+
+            }
+
+
+        }
+    }
+
+    /// <summary>
+    /// A data class for keeping track of notification details
+    /// </summary>
+    public class Notification
+    {
+        /// <summary>
+        /// The related data of what a notification is referring to, such as Assignment title that was created/graded
+        /// </summary>
+        public string NotificationTitle { get; set; }
+        /// <summary>
+        /// What type of notification it is, like 'G' for Grading or 'A' for assignment
+        /// </summary>
+        public char Type { get; set; } 
+        public string CourseName { get; set; }
+        public string Link { get; set; }
+        public DateTime NotificationDate { get; set; }
+
+        public Notification(string title, char type, string coursename, string link, DateTime notificationTime)
+        {
+            NotificationTitle = title;
+            Type = type;
+            CourseName = coursename;
+            Link = link;
+            NotificationDate = notificationTime;
         }
     }
 }
